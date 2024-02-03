16db7c696152dc2f1fb4c4aa80beb624400f5c09
yoGhastly <ve06976@innovaccion.mx>
2024-02-03 17:46:45 -0600

    fix: module not found fixed path

--- components/cart/add-to-cart.tsx
+++ components/cart/add-to-cart.tsx
@@ -2,10 +2,10 @@
 
 // import { PlusIcon } from '@heroicons/react/24/outline';
 import clsx from "clsx";
-import { addItem } from "components/cart/actions";
-import LoadingDots from "components/loading-dots";
 import { useSearchParams } from "next/navigation";
 import { useFormState, useFormStatus } from "react-dom";
+import LoadingDots from "../loading-dots";
+import { addItem } from "./actions";
 
 function SubmitButton({
   availableForSale,
@@ -81,7 +81,7 @@ export function AddToCart({
   const actionWithVariant = formAction.bind(null);
 
   return (
-    <form action={actionWithVariant}>
+    <form action={actionWithVariant as any}>
       <SubmitButton
         availableForSale={availableForSale}
         selectedVariantId={selectedVariantId}
