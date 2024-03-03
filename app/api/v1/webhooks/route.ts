80b901d3e3e0063d320f4afb63708789540f607b
yoGhastly <ve06976@innovaccion.mx>
2024-03-02 18:02:28 -0600

    feat: listen for stripe webhook event type

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -1,5 +1,4 @@
 import { supabase } from "@/app/lib/subapase/client";
-import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 import Stripe from "stripe";
 
@@ -23,25 +22,62 @@ export async function POST(request: NextRequest) {
 
   // Handle the event
   switch (event.type) {
+    case "checkout.session.async_payment_failed":
+      const checkoutSessionAsyncPaymentFailed = event.data.object;
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      return Response.json({ checkoutSessionAsyncPaymentFailed });
+    case "checkout.session.async_payment_succeeded":
+      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+      return Response.json({ checkoutSessionAsyncPaymentSucceeded });
+    case "checkout.session.completed":
+      const checkoutSessionCompleted = event.data.object;
+      return Response.json({ checkoutSessionCompleted });
+    case "checkout.session.expired":
+      const checkoutSessionExpired = event.data.object;
+      return Response.json({ checkoutSessionExpired });
+    case "payment_intent.amount_capturable_updated":
+      const paymentIntentAmountCapturableUpdated = event.data.object;
+      console.log(paymentIntentAmountCapturableUpdated);
+      return Response.json({ paymentIntentAmountCapturableUpdated });
+    case "payment_intent.canceled":
+      const paymentIntentCanceled = event.data.object;
+      console.log(paymentIntentCanceled);
+      return Response.json({ paymentIntentCanceled });
+    case "payment_intent.created":
+      const paymentIntentCreated = event.data.object;
+      console.log(paymentIntentCreated);
+      return Response.json({ paymentIntentCreated });
+    case "payment_intent.partially_funded":
+      const paymentIntentPartiallyFunded = event.data.object;
+      console.log(paymentIntentPartiallyFunded);
+      return Response.json({ paymentIntentPartiallyFunded });
+    case "payment_intent.payment_failed":
+      const paymentIntentPaymentFailed = event.data.object;
+      console.log(paymentIntentPaymentFailed);
+      return Response.json({ paymentIntentPaymentFailed });
+    case "payment_intent.processing":
+      const paymentIntentProcessing = event.data.object;
+      console.log(paymentIntentProcessing);
+      return Response.json({ paymentIntentProcessing });
+    case "payment_intent.requires_action":
+      const paymentIntentRequiresAction = event.data.object;
+      console.log(paymentIntentRequiresAction);
+      return Response.json({ paymentIntentRequiresAction });
     case "payment_intent.succeeded":
       const paymentIntentSucceeded = event.data.object;
-      cookies().set("pi_id", paymentIntentSucceeded.id);
-      try {
-        const { data, error } = await supabase
-          .from("orders")
-          .insert([paymentIntentSucceeded]);
-        if (error) {
-          console.error("Error inserting payment intent:", error.message);
-          return Response.error();
-        }
-        console.log("Payment intent saved successfully:", data);
-      } catch (error) {
-        console.error("Error inserting payment intent:", error);
-        return Response.error();
+      const { data, error } = await supabase
+        .from("orders")
+        .insert([{ paymentIntentSucceeded }]);
+      if (error) {
+        return Response.json({
+          error,
+          message: "Could not insert on orders table",
+        });
       }
-      return Response.json({ success: true, paymentIntentSucceeded });
+      return Response.json({ data });
     default:
       console.log(`Unhandled event type ${event.type}`);
-      return Response.json("Webhook stripe");
   }
+
+  return Response.json("Webhook stripe");
 }
