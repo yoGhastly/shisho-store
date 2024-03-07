35b7f2d4c2cde5ec463e2688d3cd6b29c7d99873
yoGhastly <ve06976@innovaccion.mx>
2024-03-06 22:02:36 -0600

    feat: listen for checkout sessions and payment intents at webhook endpoint
    - passing raw body to constructEvent as it is the required type on stripe dashboard

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -5,22 +5,67 @@ const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET || "";
 
 export async function POST(request: NextRequest) {
   const sig = request.headers.get("stripe-signature") || "";
-  const body = await request.json();
+  const rawBody = await request.text();
 
   let event: Stripe.Event;
 
   try {
-    event = Stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    event = Stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
   } catch (err) {
     return Response.json({ message: `Webhook Error: ${err}`, status: 400 });
   }
 
-  if (event.type === "checkout.session.completed") {
-    console.log("INIT CHECKOUT COMPLETED");
-    const checkoutSessionCompleted = event.data.object;
-    console.log("hit event checkout completed", checkoutSessionCompleted);
+  console.log("AFTER SWITCH EVENT");
 
-    return Response.json({ message: "Payment succeeded", status: 200 });
+  switch (event.type) {
+    case "checkout.session.async_payment_failed":
+      const checkoutSessionAsyncPaymentFailed = event.data.object;
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      return Response.json({ checkoutSessionAsyncPaymentFailed });
+    case "checkout.session.async_payment_succeeded":
+      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+      return Response.json({ checkoutSessionAsyncPaymentSucceeded });
+    case "checkout.session.completed":
+      const checkoutSessionCompleted = event.data.object;
+      console.log("HIT CHECKOUT COMPLETED");
+      return Response.json({ checkoutSessionCompleted });
+    case "checkout.session.expired":
+      const checkoutSessionExpired = event.data.object;
+      return Response.json({ checkoutSessionExpired });
+    case "payment_intent.amount_capturable_updated":
+      const paymentIntentAmountCapturableUpdated = event.data.object;
+      console.log(paymentIntentAmountCapturableUpdated);
+      return Response.json({ paymentIntentAmountCapturableUpdated });
+    case "payment_intent.canceled":
+      const paymentIntentCanceled = event.data.object;
+      console.log(paymentIntentCanceled);
+      return Response.json({ paymentIntentCanceled });
+    case "payment_intent.created":
+      const paymentIntentCreated = event.data.object;
+      console.log(paymentIntentCreated);
+      return Response.json({ paymentIntentCreated });
+    case "payment_intent.partially_funded":
+      const paymentIntentPartiallyFunded = event.data.object;
+      console.log(paymentIntentPartiallyFunded);
+      return Response.json({ paymentIntentPartiallyFunded });
+    case "payment_intent.payment_failed":
+      const paymentIntentPaymentFailed = event.data.object;
+      console.log(paymentIntentPaymentFailed);
+      return Response.json({ paymentIntentPaymentFailed });
+    case "payment_intent.processing":
+      const paymentIntentProcessing = event.data.object;
+      console.log(paymentIntentProcessing);
+      return Response.json({ paymentIntentProcessing });
+    case "payment_intent.requires_action":
+      const paymentIntentRequiresAction = event.data.object;
+      console.log(paymentIntentRequiresAction);
+      return Response.json({ paymentIntentRequiresAction });
+    case "payment_intent.succeeded":
+      const paymentIntentSucceeded = event.data.object;
+      console.log("HIT CHECKOUT COMPLETED");
+      return Response.json({ paymentIntentSucceeded });
+    default:
+      console.log(`Unhandled event type ${event.type}`);
   }
 
   return Response.json({
