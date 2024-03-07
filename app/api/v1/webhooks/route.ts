372c327450f522795f4ec829b2318923f7b4a68a
yoGhastly <ve06976@innovaccion.mx>
2024-03-07 17:24:39 -0600

    feat: passing raw body and signature from header

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -9,32 +9,54 @@ const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET || "";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
-  console.log("hit webhook");
-  const sig = request.headers.get("stripe-signature")!;
-  const rawBody = await request.text();
-
-  let event: Stripe.Event;
-
   try {
-    event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
-  } catch (err) {
-    const errorMessage = err instanceof Error ? err.message : "Unknown error";
-    // On error, log and return the error message.
-    if (err! instanceof Error) console.log(err);
-    console.log(`❌ Error message: ${errorMessage}`);
+    const rawBody = await request.text();
+    const sig = request.headers.get("stripe-signature") || "";
 
+    console.log(`sig ${sig}`);
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : "Unknown error";
+      // On error, log and return the error message.
+      if (err! instanceof Error) console.log(err);
+      console.log(`❌ Error message: ${errorMessage}`);
+
+      return NextResponse.json(
+        {
+          error: {
+            message: `Webhook Error: ${errorMessage}`,
+          },
+        },
+        { status: 400 },
+      );
+    }
+
+    // Successfully constructed event.
+    console.log("✅ Success:", event.id);
+
+    switch (event.type) {
+      case "checkout.session.completed":
+        console.log("checkout completed");
+        break;
+      default:
+        console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+        break;
+    }
+
+    // Return a response to acknowledge receipt of the event.
+    return NextResponse.json({ received: true });
+  } catch {
     return NextResponse.json(
       {
         error: {
-          message: `Webhook Error: ${errorMessage}`,
+          message: `Method Not Allowed`,
         },
       },
-      { status: 400 },
-    );
+      { status: 405 },
+    ).headers.set("Allow", "POST");
   }
-
-  // Successfully constructed event.
-  console.log("✅ Success:", event.id);
-
-  return new Response(null, { status: 200 });
 }
