028cb6f49ac3f924608a53691a9b13ea6878a931
yoGhastly <ve06976@innovaccion.mx>
2024-03-06 19:48:39 -0600

    feat: listen for checkout session completed event type

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { NextRequest } from "next/server";
 import Stripe from "stripe";
 
@@ -7,7 +6,6 @@ const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET || "";
 export async function POST(request: NextRequest) {
   const sig = request.headers.get("stripe-signature") || "";
   const body = await request.json();
-  console.log("BODY", body);
 
   let event: Stripe.Event;
 
@@ -17,12 +15,11 @@ export async function POST(request: NextRequest) {
     return Response.json({ message: `Webhook Error: ${err}`, status: 400 });
   }
 
-  // Handle the event
-  if (event.type === "payment_intent.succeeded") {
-    const paymentIntentSucceeded = event.data.object;
-    console.log(paymentIntentSucceeded);
+  if (event.type === "checkout.session.completed") {
+    console.log("INIT CHECKOUT COMPLETED");
+    const checkoutSessionCompleted = event.data.object;
+    console.log("hit event checkout completed", checkoutSessionCompleted);
 
-    await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/send`);
     return Response.json({ message: "Payment succeeded", status: 200 });
   }
 
