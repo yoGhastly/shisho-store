f6e9350692c78031078e974a5e272aa22d374c55
yoGhastly <ve06976@innovaccion.mx>
2024-03-06 22:21:51 -0600

    feat: remove unused event types

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -15,57 +15,10 @@ export async function POST(request: NextRequest) {
     return Response.json({ message: `Webhook Error: ${err}`, status: 400 });
   }
 
-  console.log("AFTER SWITCH EVENT");
-
-  switch (event.type) {
-    case "checkout.session.async_payment_failed":
-      const checkoutSessionAsyncPaymentFailed = event.data.object;
-      // Then define and call a function to handle the event checkout.session.async_payment_failed
-      return Response.json({ checkoutSessionAsyncPaymentFailed });
-    case "checkout.session.async_payment_succeeded":
-      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
-      return Response.json({ checkoutSessionAsyncPaymentSucceeded });
-    case "checkout.session.completed":
-      const checkoutSessionCompleted = event.data.object;
-      console.log("HIT CHECKOUT COMPLETED");
-      return Response.json({ checkoutSessionCompleted });
-    case "checkout.session.expired":
-      const checkoutSessionExpired = event.data.object;
-      return Response.json({ checkoutSessionExpired });
-    case "payment_intent.amount_capturable_updated":
-      const paymentIntentAmountCapturableUpdated = event.data.object;
-      console.log(paymentIntentAmountCapturableUpdated);
-      return Response.json({ paymentIntentAmountCapturableUpdated });
-    case "payment_intent.canceled":
-      const paymentIntentCanceled = event.data.object;
-      console.log(paymentIntentCanceled);
-      return Response.json({ paymentIntentCanceled });
-    case "payment_intent.created":
-      const paymentIntentCreated = event.data.object;
-      console.log(paymentIntentCreated);
-      return Response.json({ paymentIntentCreated });
-    case "payment_intent.partially_funded":
-      const paymentIntentPartiallyFunded = event.data.object;
-      console.log(paymentIntentPartiallyFunded);
-      return Response.json({ paymentIntentPartiallyFunded });
-    case "payment_intent.payment_failed":
-      const paymentIntentPaymentFailed = event.data.object;
-      console.log(paymentIntentPaymentFailed);
-      return Response.json({ paymentIntentPaymentFailed });
-    case "payment_intent.processing":
-      const paymentIntentProcessing = event.data.object;
-      console.log(paymentIntentProcessing);
-      return Response.json({ paymentIntentProcessing });
-    case "payment_intent.requires_action":
-      const paymentIntentRequiresAction = event.data.object;
-      console.log(paymentIntentRequiresAction);
-      return Response.json({ paymentIntentRequiresAction });
-    case "payment_intent.succeeded":
-      const paymentIntentSucceeded = event.data.object;
-      console.log("HIT CHECKOUT COMPLETED");
-      return Response.json({ paymentIntentSucceeded });
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+  // Handle the checkout.session.completed event
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
+    console.log("SESSION: ", session);
   }
 
   return Response.json({
