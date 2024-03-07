1dd26d50292fea6a812124d805a9ac6eeb4511eb
yoGhastly <ve06976@innovaccion.mx>
2024-03-07 16:26:01 -0600

    feat: implement resend api on webhook after checkout is completed

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -1,8 +1,12 @@
 import { stripe } from "@/app/lib/stripe/server";
+import { Order } from "@/app/types";
 import { NextRequest } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
+import { EmailTemplate } from "@/components/email-template";
 
 const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET || "";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   console.log("hit webhook");
@@ -20,8 +24,33 @@ export async function POST(request: NextRequest) {
   switch (event.type) {
     case "checkout.session.completed":
       const checkoutSessionCompleted = event.data.object;
-      console.log({ checkoutSessionCompleted });
-      break;
+
+      const order: Order = {
+        id: "399448884",
+        items: [{ id: "00933", name: "test", qty: 1, amount: 1000 }],
+        customer: {
+          email: "diego.espinosagrc@uanl.edu.mx",
+          name: "Jose Diego Espinosa Garcia",
+          phone: "0504500393",
+        },
+        shipping: {
+          city: "AED",
+        },
+      };
+
+      try {
+        const data = await resend.emails.send({
+          from: "Acme <onboarding@resend.dev>",
+          to: [order.customer.email],
+          subject: `Confirmation Order #${order.id}`,
+          react: EmailTemplate({ order }),
+          text: `Confirmation Order #${order.id}`,
+        });
+
+        return Response.json(data);
+      } catch (error) {
+        return Response.json({ error });
+      }
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
