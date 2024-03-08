6880387bb35b09ab082b86e9633309decd52f207
yoGhastly <ve06976@innovaccion.mx>
2024-03-08 17:49:07 -0600

    feat: retrieving stripe signature from headerList form next/headers

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -1,17 +1,21 @@
 import { stripe } from "@/app/lib/stripe/server";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET || "";
+const endpointSecret = process.env.whsec_iwhXRGDPaOAfGFaCCMolP3IItvymeFSf || "";
 
 export async function POST(request: NextRequest) {
-  const buf = await request.text();
-  const sig = request.headers.get("stripe-signature")!;
+  const body = await request.text();
+  const headerList = headers();
+  const sig = headerList.get("stripe-signature") || "";
+
+  console.log(sig);
 
   let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(buf, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
     const errorMessage = err instanceof Error ? err.message : "Unknown error";
     // On error, log and return the error message.
@@ -28,55 +32,20 @@ export async function POST(request: NextRequest) {
     );
   }
 
+  console.log(event);
+
   // Handle the event
   switch (event.type) {
-    case "checkout.session.async_payment_failed":
-      const checkoutSessionAsyncPaymentFailed = event.data.object;
-      // Then define and call a function to handle the event checkout.session.async_payment_failed
-      return Response.json({ checkoutSessionAsyncPaymentFailed });
-    case "checkout.session.async_payment_succeeded":
-      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
-      return Response.json({ checkoutSessionAsyncPaymentSucceeded });
     case "checkout.session.completed":
       const checkoutSessionCompleted = event.data.object;
-      return Response.json({ checkoutSessionCompleted });
-    case "checkout.session.expired":
-      const checkoutSessionExpired = event.data.object;
-      return Response.json({ checkoutSessionExpired });
-    case "payment_intent.amount_capturable_updated":
-      const paymentIntentAmountCapturableUpdated = event.data.object;
-      console.log(paymentIntentAmountCapturableUpdated);
-      return Response.json({ paymentIntentAmountCapturableUpdated });
-    case "payment_intent.canceled":
-      const paymentIntentCanceled = event.data.object;
-      console.log(paymentIntentCanceled);
-      return Response.json({ paymentIntentCanceled });
-    case "payment_intent.created":
-      const paymentIntentCreated = event.data.object;
-      console.log(paymentIntentCreated);
-      return Response.json({ paymentIntentCreated });
-    case "payment_intent.partially_funded":
-      const paymentIntentPartiallyFunded = event.data.object;
-      console.log(paymentIntentPartiallyFunded);
-      return Response.json({ paymentIntentPartiallyFunded });
-    case "payment_intent.payment_failed":
-      const paymentIntentPaymentFailed = event.data.object;
-      console.log(paymentIntentPaymentFailed);
-      return Response.json({ paymentIntentPaymentFailed });
-    case "payment_intent.processing":
-      const paymentIntentProcessing = event.data.object;
-      console.log(paymentIntentProcessing);
-      return Response.json({ paymentIntentProcessing });
-    case "payment_intent.requires_action":
-      const paymentIntentRequiresAction = event.data.object;
-      console.log(paymentIntentRequiresAction);
-      return Response.json({ paymentIntentRequiresAction });
-    case "payment_intent.succeeded":
-      const paymentIntentSucceeded = event.data.object;
-      return Response.json({ paymentIntentSucceeded });
+      console.log({ checkoutSessionCompleted });
+      // Then define and call a function to handle the event checkout.session.completed
+      break;
+    // ... handle other event types
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
 
-  return Response.json("Webhook stripe");
+  // Return a 200 response to acknowledge receipt of the event
+  return new Response(null, { status: 200 });
 }
