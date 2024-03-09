21f55d3c4f2de7518fc92a255c9f686540c3f94b
yoGhastly <ve06976@innovaccion.mx>
2024-03-08 18:39:15 -0600

    fix: typo on env variable for wh secret

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -6,7 +6,7 @@ const relevantEvents = new Set(["checkout.session.completed"]);
 export async function POST(req: Request) {
   const body = await req.text();
   const sig = req.headers.get("stripe-signature") as string;
-  const webhookSecret = process.env.STRIPE_ENDPOINT_SECRET;
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   console.log({ webhookSecret });
   let event: Stripe.Event;
 
