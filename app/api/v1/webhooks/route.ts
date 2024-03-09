f3c0734ab5c68a280ce036e62cda0f249a76c460
yoGhastly <ve06976@innovaccion.mx>
2024-03-08 18:09:20 -0600

    feat: listen for relevant events in webhook route.
    - log the type of event is receiving
    - handle success and error checkout completed event type

--- app/api/v1/webhooks/route.ts
+++ app/api/v1/webhooks/route.ts
@@ -1,51 +1,47 @@
 import { stripe } from "@/app/lib/stripe/server";
-import { headers } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET || "";
+const relevantEvents = new Set(["checkout.session.completed"]);
 
-export async function POST(request: NextRequest) {
-  const body = await request.text();
-  const headerList = headers();
-  const sig = headerList.get("stripe-signature") || "";
-
-  console.log(sig);
-
+export async function POST(req: Request) {
+  const body = await req.text();
+  const sig = req.headers.get("stripe-signature") as string;
+  const webhookSecret = process.env.STRIPE_ENDPOINT_SECRET;
   let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-  } catch (err) {
-    const errorMessage = err instanceof Error ? err.message : "Unknown error";
-    // On error, log and return the error message.
-    if (err! instanceof Error) console.log(err);
-    console.log(`❌ Error message: ${errorMessage}`);
+    if (!sig || !webhookSecret)
+      return new Response("Webhook secret not found.", { status: 400 });
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    console.log(`🔔  Webhook received: ${event.type}`);
+  } catch (err: any) {
+    console.log(`❌ Error message: ${err.message}`);
+    return new Response(`Webhook Error: ${err.message}`, { status: 400 });
+  }
 
-    return NextResponse.json(
-      {
-        error: {
-          message: `Webhook Error: ${errorMessage}`,
+  if (relevantEvents.has(event.type)) {
+    try {
+      switch (event.type) {
+        case "checkout.session.completed":
+          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+          console.log("checkout completed ✅");
+          break;
+        default:
+          throw new Error("Unhandled relevant event!");
+      }
+    } catch (error) {
+      console.log(error);
+      return new Response(
+        "Webhook handler failed. View your Next.js function logs.",
+        {
+          status: 400,
         },
-      },
-      { status: 400 },
-    );
+      );
+    }
+  } else {
+    return new Response(`Unsupported event type: ${event.type}`, {
+      status: 400,
+    });
   }
-
-  console.log(event);
-
-  // Handle the event
-  switch (event.type) {
-    case "checkout.session.completed":
-      const checkoutSessionCompleted = event.data.object;
-      console.log({ checkoutSessionCompleted });
-      // Then define and call a function to handle the event checkout.session.completed
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-  return new Response(null, { status: 200 });
+  return new Response(JSON.stringify({ received: true }));
 }
