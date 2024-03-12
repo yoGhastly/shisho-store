fda0ebb6e62313c6c4cf93d7fd1c9bea66a24a3d
yoGhastly <ve06976@innovaccion.mx>
2024-03-11 22:23:50 -0600

    fix: implement new order interface structure for email template.
    - method items is not in order interface for now

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -10,18 +10,17 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   order,
 }) => (
   <div className="font-sans flex gap-10 flex-col">
-    <h1 className="font-bold">Welcome, {order.customer.name}!</h1>
+    <h1 className="font-bold">Welcome, {order.customerName}!</h1>
     <p>Your confirmation of order #{order.id}</p>
     <div className="flex flex-col gap-5 items-center">
       <p className="font-bold">Order Details</p>
       <ul>
-        {order.items.map((i) => (
-          <li key={i.id} className="flex flex-col gap-3">
-            <p className="font-bold">{i.name}</p>
-            <p className="text-xs text-[#d4d4d4]">{i.qty}</p>
-            <p className="font-medium">{i.amount}</p>
-          </li>
-        ))}
+        <li>
+          {order.amountTotal} {order.currency}
+        </li>
+        <li>{order.shippingAddress.line1}</li>
+        <li>{order.customerEmail}</li>
+        <li>{order.customerPhone}</li>
       </ul>
     </div>
   </div>
