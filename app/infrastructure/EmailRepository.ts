cef81a6a5c9cbc6c444652802cdd5ae7a671691c
yoGhastly <ve06976@innovaccion.mx>
2024-03-12 00:15:11 -0600

    feat: test for order passing to sendConfirmationEmail method

--- app/infrastructure/EmailRepository.ts
+++ app/infrastructure/EmailRepository.ts
@@ -11,7 +11,7 @@ export class EmailRepository {
 
   async sendOrderConfirmation(order: Order): Promise<void> {
     try {
-      const { data, error } = await this.resend.emails.send({
+      /* const { data, error } = await this.resend.emails.send({
         from: "Acme <onboarding@resend.dev>",
         to: [order.customerEmail],
         subject: `Confirmation Order #${order.id}`,
@@ -21,8 +21,8 @@ export class EmailRepository {
 
       if (error) {
         console.error(`Resend error: ${error.message}`);
-      }
-      console.log("Email sent successfully:", data);
+      } */
+      console.log("Email sent successfully:", order);
     } catch (error: any) {
       console.error("Error sending email:", error);
       throw new Error(`Failed to send email confirmation: ${error.message}`);
