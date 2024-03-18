d2f92cec8a92ceeb7c11ae36cf8dff31883f0845
yoGhastly <ve06976@innovaccion.mx>
2024-03-17 19:43:47 -0600

    fix: orderId type

--- app/domain/SearchOrder.ts
+++ app/domain/SearchOrder.ts
@@ -1,10 +1,9 @@
 import { OrderRepository } from "../infrastructure/OrderRepository";
-import { Order } from "../types";
 
 export class OrderSearch {
   constructor(private readonly repository: Pick<OrderRepository, "search">) { }
 
-  async search(orderId: Pick<Order, "id">): Promise<void | null> {
+  async search(orderId: string): Promise<void | null> {
     await this.repository.search(orderId);
   }
 }
