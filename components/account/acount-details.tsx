177f114a62afc23da2cc4c98c45a32cf27df21f8
yoGhastly <ve06976@innovaccion.mx>
2024-03-20 17:58:35 -0600

    feat: redirect user to /account if logged in, otherwise redirect it to /orders

--- components/account/acount-details.tsx
+++ components/account/acount-details.tsx
@@ -1,8 +1,8 @@
-"use client";
-import React from "react";
-import { Avatar } from "../avatar";
-import { useClerk } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+'use client';
+import React from 'react';
+import { Avatar } from '../avatar';
+import { useClerk } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
 
 type Details = {
   emailAddress: string;
@@ -11,24 +11,29 @@ type Details = {
 
 interface Props {
   details: Details;
+  showLogOutButton: boolean;
 }
 
-export function AccountDetails({ details }: Props) {
+export function AccountDetails({ details, showLogOutButton }: Props) {
   const { signOut } = useClerk();
   const router = useRouter();
 
   return (
     <div className="flex gap-5">
-      <Avatar name={details.name ?? "Guest"} />
+      <Avatar name={details.name ?? 'Guest'} />
       <div className="flex flex-col gap-1.5">
-        <p className="uppercase font-bold text-xl">{details.name ?? "Guest"}</p>
+        <p className="uppercase font-bold text-xl">
+          {details.name ?? 'Guest'}
+        </p>
         <p>{details.emailAddress}</p>
-        <button
-          onClick={() => signOut(() => router.push("/"))}
-          className="underline self-start hover:no-underline transition delay-75 hover:text-black/65"
-        >
-          Log out
-        </button>
+        {showLogOutButton && (
+          <button
+            onClick={() => signOut(() => router.push('/'))}
+            className="underline self-start hover:no-underline transition delay-75 hover:text-black/65"
+          >
+            Log out
+          </button>
+        )}
       </div>
     </div>
   );
