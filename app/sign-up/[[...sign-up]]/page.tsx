42f6153a9b2d3f3f4f393836dffcd668fd3f29ef
yoGhastly <ve06976@innovaccion.mx>
2024-03-19 18:53:53 -0600

    feat: hide site logo on sign-up page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,22 +5,22 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="h-screen flex flex-col md:flex-row justify-center items-center gap-8 p-5">
+    <div className="h-screen flex flex-col md:flex-row justify-center items-center gap-8">
       <section
         className={clsx(
-          "w-full h-full flex justify-center items-center bg-secondary/[0.2]",
+          "w-full h-full flex justify-center items-center bg-secondary/[0.2] p-3",
         )}
       >
         <div className="flex flex-col max-w-lg gap-3">
-          <h1 className="font-semibold text-center text-3xl">
+          <h1 className="font-semibold text-center text-xl md:text-3xl">
             Sign Up to view your orders.
           </h1>
           <p className="text-center text-[#4D5768]">
-            Sign up to access your order history, track your shipments, and
+            Sign Up to access your order history, track your shipments, and
             manage your account details. Once signed in, you can view all your
             past orders and easily reorder your favorite items.
           </p>
-          <figure className="w-52 h-52 relative self-center">
+          <figure className="w-14 h-14 md:w-52 md:h-52 relative self-center hidden md:block">
             <Image
               src="/logo_2.svg"
               fill
