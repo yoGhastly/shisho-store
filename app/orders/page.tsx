c6e08d1a70c8e25b689a324b628433e8db974252
yoGhastly <ve06976@innovaccion.mx>
2024-03-20 17:57:37 -0600

    feat: add absolute url to redirectToSignIn clerk method.
    - show log out button if user is logged in, otherwise hide it

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,18 +1,19 @@
-"use server";
-import React from "react";
-import { AccountDetails } from "@/components/account/acount-details";
-import { AddressBook } from "@/components/account/address-book";
-import { ReturnItem } from "@/components/account/return-item";
-import { OrderItem } from "@/components/account/order-item";
-import { BreadCrumb } from "./breadcrumb";
-import { currentUser, redirectToSignIn } from "@clerk/nextjs";
-import { OrderSearchCriteria } from "../infrastructure/criteria/OrderSearchCriteria";
-import { SupabaseOrderRepository } from "./order-repository";
-import { Order } from "../types";
-import { Skeleton } from "@/components/skeleton";
-import { ShoppingBagIcon } from "@heroicons/react/24/outline";
-import { Button } from "@nextui-org/react";
-import Footer from "@/components/layout/footer";
+'use server';
+import React from 'react';
+import { AccountDetails } from '@/components/account/acount-details';
+import { AddressBook } from '@/components/account/address-book';
+import { ReturnItem } from '@/components/account/return-item';
+import { OrderItem } from '@/components/account/order-item';
+import { BreadCrumb } from './breadcrumb';
+import { currentUser, redirectToSignIn } from '@clerk/nextjs';
+import { OrderSearchCriteria } from '../infrastructure/criteria/OrderSearchCriteria';
+import { SupabaseOrderRepository } from './order-repository';
+import { Order } from '../types';
+import { Skeleton } from '@/components/skeleton';
+import { ShoppingBagIcon } from '@heroicons/react/24/outline';
+import { Button } from '@nextui-org/react';
+import Footer from '@/components/layout/footer';
+import Link from 'next/link';
 
 type Details = {
   emailAddress: string;
@@ -29,14 +30,16 @@ export default async function Orders() {
   };
 
   let details = {
-    name: "Guest",
+    name: 'Guest',
     emailAddress: criteria.customerName,
   };
-  let address = {} as Pick<Order, "shippingAddress"> &
-    Pick<Order, "customerName">;
+  let address = {} as Pick<Order, 'shippingAddress'> &
+    Pick<Order, 'customerName'>;
 
   if (!user) {
-    redirectToSignIn({ returnBackUrl: "/orders" });
+    redirectToSignIn({
+      returnBackUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/orders`,
+    });
   }
 
   const orders = await orderRepository.searchBy(criteria);
@@ -55,17 +58,22 @@ export default async function Orders() {
 
   return (
     <React.Fragment>
-      <section className="flex flex-col h-screen md:gap-5 justify-center items-center">
-        <header className="flex self-start w-full p-5 max-w-6xl mx-auto mt-10 md:mt-24">
+      <section className="flex flex-col h-full md:gap-5 justify-center items-center">
+        <header className="flex self-start w-full p-5 max-w-6xl mx-auto mt-5">
           <div>
-            <BreadCrumb values={[{ label: "Your Account", url: "/orders" }]} />
+            <BreadCrumb
+              values={[{ label: 'Your Account', url: '/orders' }]}
+            />
           </div>
         </header>
 
         <article className="flex flex-col md:flex-row gap-5 w-full p-5 h-full max-w-6xl mx-auto">
           <aside className="h-full rounded-sm basis-1/3 flex flex-col gap-10">
             <Skeleton loaded={details ? true : false}>
-              <AccountDetails details={details as Details} />
+              <AccountDetails
+                details={details as Details}
+                showLogOutButton={user ? true : false}
+              />
             </Skeleton>
             {orders.length > 0 && (
               <Skeleton loaded={address ? true : false}>
@@ -75,16 +83,30 @@ export default async function Orders() {
             <ReturnItem />
           </aside>
           <section className="rounded-sm h-full basis-full p-5 flex flex-col gap-5">
-            <h2 className="uppercase font-bold text-2xl">Your Orders</h2>
+            <h2 className="uppercase font-bold text-2xl">
+              Your Orders
+            </h2>
             {orders.length > 0 ? (
-              orders.map((order) => <OrderItem order={order} key={order.id} />)
+              orders.map((order) => (
+                <OrderItem order={order} key={order.id} />
+              ))
             ) : (
               <section className="flex flex-col justify-center items-center gap-8 my-auto">
                 <ShoppingBagIcon className="h-10" />
                 <div className="flex flex-col gap-3 text-center">
-                  <p className="font-semibold">Your orders are empty.</p>
-                  <p className="text-[#4D5768]">
-                    Looks like you haven&apos;t made any purchases yet. Start
+                  <p className="font-semibold">
+                    Your orders are empty.
+                  </p>
+                  <p className="text-[#4D5768] max-w-sm">
+                    Looks like you haven&apos;t made any
+                    purchases yet or{' '}
+                    <Link
+                      className="text-primary"
+                      href="/sign-in"
+                    >
+                      Sign In{' '}
+                    </Link>
+                    to view your past orders . Start
                     shopping now!
                   </p>
                 </div>
