271e0752cb76361ed21a402fa03f514f1193697a
yoGhastly <ve06976@innovaccion.mx>
2024-03-21 01:06:57 -0600

    feat: show text or status.
    - if text is passed then render it, otherwise show status

--- app/orders/chip-status.tsx
+++ app/orders/chip-status.tsx
@@ -1,26 +1,27 @@
-"use client";
+'use client';
 
-import { Chip } from "@nextui-org/react";
-import { useMediaQuery } from "../hooks/useMediaQuery";
+import { Chip } from '@nextui-org/react';
+import { useMediaQuery } from '../hooks/useMediaQuery';
 
-type Status = "In progress" | "Idle" | "No status";
+export type Status = 'In progress' | 'Idle' | 'No status';
 
 interface Props {
   status: Status;
+  text?: string;
 }
 
-export function StatusChip({ status }: Props) {
+export function StatusChip({ status, text }: Props) {
   const isSm = useMediaQuery(480);
   const colorStatus =
-    status === "In progress"
-      ? "success"
-      : status === "Idle"
-        ? "warning"
-        : "default";
+    status === 'In progress'
+      ? 'success'
+      : status === 'Idle'
+        ? 'warning'
+        : 'default';
 
   return (
-    <Chip color={colorStatus} variant="flat" size={isSm ? "sm" : "md"}>
-      {status}
+    <Chip color={colorStatus} variant="flat" size={isSm ? 'sm' : 'md'}>
+      {text ? text : status}
     </Chip>
   );
 }
