1677b213688714a2c1f4134981f87d5a2dff8010
yoGhastly <ve06976@innovaccion.mx>
2024-03-20 18:00:32 -0600

    feat: validate user name.
    - if user is a guest then redirect it to /orders, otherwise to /account

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,20 +1,21 @@
-"use client";
-import { useMediaQuery } from "@/app/hooks/useMediaQuery";
-import { Avatar as NextUIAvatar } from "@nextui-org/react";
+'use client';
+import { useMediaQuery } from '@/app/hooks/useMediaQuery';
+import { Avatar as NextUIAvatar } from '@nextui-org/react';
 
 export function Avatar({ name }: { name: string }) {
   const isSm = useMediaQuery(480);
+  const GUEST_NAME = 'Guest';
   return (
     <NextUIAvatar
       name={name}
       as="a"
-      href="/account"
+      href={name === GUEST_NAME ? '' : '/account'}
       tw="text-white"
-      size={isSm ? "md" : "lg"}
+      size={isSm ? 'md' : 'lg'}
       color="secondary"
       getInitials={(name) => {
-        const words = name.split(" ");
-        const initials = words.map((word) => word.charAt(0)).join("");
+        const words = name.split(' ');
+        const initials = words.map((word) => word.charAt(0)).join('');
         return initials.slice(0, 2).toUpperCase();
       }}
     />
