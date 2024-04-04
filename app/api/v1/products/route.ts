3e5101d576edf3ff295b4122d70212a53e0a179d
yoGhastly <ve06976@innovaccion.mx>
2024-04-03 19:44:37 -0600

    feat: home view revalidatePath

--- app/api/v1/products/route.ts
+++ app/api/v1/products/route.ts
@@ -1,27 +1,30 @@
-import Stripe from "stripe";
+import { revalidatePath } from 'next/cache';
+import Stripe from 'stripe';
 
 const STRIPE_SECRET_KEY =
-  "sk_test_51OfZDtKrqv6TAD3CJhDV05wPUkgg7625mrQpm2IDAN0eSfIl6JN0o0tgft2KBmIPBQfMNX2xYknXfraMxxsgLw5J009SBO2FUp";
+    'sk_test_51OfZDtKrqv6TAD3CJhDV05wPUkgg7625mrQpm2IDAN0eSfIl6JN0o0tgft2KBmIPBQfMNX2xYknXfraMxxsgLw5J009SBO2FUp';
 const stripe = new Stripe(STRIPE_SECRET_KEY);
 
 export async function GET() {
-  try {
-    const { data: products } = await stripe.products.list({
-      limit: 300,
-    });
+    try {
+        const { data: products } = await stripe.products.list({
+            limit: 300,
+        });
 
-    return Response.json({
-      products: products,
-      success: true,
-      status: 200,
-    });
-  } catch (error) {
-    console.error(error);
-    return Response.json({
-      error: "Error retrieving products",
-      products: [],
-      success: false,
-      status: 500,
-    });
-  }
+        revalidatePath('/', 'page');
+
+        return Response.json({
+            products: products,
+            success: true,
+            status: 200,
+        });
+    } catch (error) {
+        console.error(error);
+        return Response.json({
+            error: 'Error retrieving products',
+            products: [],
+            success: false,
+            status: 500,
+        });
+    }
 }
