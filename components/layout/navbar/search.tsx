2add8f1e844b6ab8816a1fa1d4b7230964ae06be
Diego Espinosa <78836469+yoGhastly@users.noreply.github.com>
2024-04-03 20:18:09 -0600

    Update search.tsx

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -36,7 +36,7 @@ export default function Search() {
         placeholder="Search for products..."
         autoComplete="off"
         defaultValue={searchParams?.get("q") || ""}
-        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500"
+        className="w-full rounded-lg border text-medium bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500"
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <Image
