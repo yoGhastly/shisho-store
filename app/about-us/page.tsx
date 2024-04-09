ca55e58205f6fcb432f28b6333d2a6450dde38b0
yoGhastly <ve06976@innovaccion.mx>
2024-04-08 19:11:05 -0600

    feat: modify html format by justifying text and put a new line

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,32 +1,32 @@
-import Footer from "@/components/layout/footer";
-import Prose from "@/components/prose";
-import styles from "../../styles/Logo.module.css";
-import clsx from "clsx";
+import Footer from '@/components/layout/footer';
+import Prose from '@/components/prose';
+import styles from '../../styles/Logo.module.css';
+import clsx from 'clsx';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 const html = `
 Welcome to Shisho Baby Clothes - Adorable Fashion for Little Ones!
 
 At Shisho Baby clothes, we believe in making every moment with your little one extra special. Our passion for creating adorable, comfortable, and high-quality baby clothes stems from our commitment to providing parents with the best for their bundles of joy.
 
-Who We Are:
+Who We Are
 
 Founded in 2023, Shisho Baby Clothes is a family-owned business dedicated to dressing your little ones in style. We understand the importance of those precious early years and strive to make them even more memorable with our charming and practical baby clothing collections.
 
-Our Mission:
+Our Mission
 
 Our mission is simple - to bring joy to families by offering a delightful range of baby clothes that combine style, comfort, and affordability. We believe that every baby deserves to be wrapped in love and dressed in the cutest outfits.
 
-Connect With Us:
+Connect With Us
 We love hearing from our customers! Connect with us on social media platforms for the latest updates. Your stories and experiences inspire us to keep doing what we love.
 
 Thank you for choosing Shisho Baby Clothes for your baby's wardrobe. We look forward to being part of your family's journey!
 `;
 
 const htmlWithBold = html.replace(
-  /(Who We Are:|Our Mission:|Connect With Us:)/g,
-  "<br/><br/><strong>$1</strong>",
+  /(Who We Are|Our Mission|Connect With Us)/g,
+  '<br/><br/><strong>$1</strong><br/>',
 );
 
 export default function Page() {
@@ -36,7 +36,7 @@ export default function Page() {
         <div className="max-w-2xl py-20 mx-auto">
           <h1
             className={clsx(
-              "mb-8 text-3xl md:text-5xl font-bold",
+              'mb-8 text-3xl md:text-5xl font-bold',
               styles.magic,
             )}
           >
--- app/shipping-policy/page.tsx
+++ app/shipping-policy/page.tsx
@@ -1,15 +1,15 @@
-import Footer from "@/components/layout/footer";
-import Prose from "@/components/prose";
-import styles from "../../styles/Logo.module.css";
-import clsx from "clsx";
+import Footer from '@/components/layout/footer';
+import Prose from '@/components/prose';
+import styles from '../../styles/Logo.module.css';
+import clsx from 'clsx';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 const html = `
-1. Processing Time:
+1. Processing Time
 
 Orders are typically processed within 2 business days. Please note that processing time may vary during peak seasons.
-2. Shipping Rates:
+2. Shipping Rates
 
 We offer FREE delivery on orders from AED 250 and above.
 
@@ -17,25 +17,25 @@ Orders under AED 250 will include a delivery charge during checkout, based on th
 
 Dubai 40 aed
 Sharjah 50 aed
-Abu Dhabi, Ajman, Al Ain, Umm Al Quwain, Ras Al Khaima, Fujairah 60 aed
+Abu Dhabi, Ajman, Al Ain, Umm Al Quwain, Ras Al Khaima, Fujairah 60 aed.
 
 
-3. Shipment Tracking:
+3. Shipment Tracking
 
 Once your order has been shipped, you will receive a confirmation email with tracking information. You can track your shipment using the provided tracking number.
 
-4. Shipping Restrictions:
+4. Shipping Restrictions
 
 We currently only ship to addresses within United Arab Emirates.
 
-5. Lost or Stolen Packages:
+5. Lost or Stolen Packages
 
 We are not responsible for lost or stolen packages. Please ensure that the shipping address provided is secure and accurate.
 `;
 
 const formattedHtml = html.replace(
-  /^(\d+)\.(.*)/gm,
-  "<br/><br/><strong>$1.$2</strong>",
+  /(1. Processing Time|2. Shipping Rates|3. Shipment Tracking|4. Shipment Restrictions|5. Lost or Stolen Packages)/g,
+  '<br/><br/><strong>$1</strong><br/>',
 );
 
 export default function Page() {
@@ -45,13 +45,13 @@ export default function Page() {
         <div className="max-w-2xl py-20 mx-auto">
           <h1
             className={clsx(
-              "mb-8 text-3xl md:text-5xl font-bold",
+              'mb-8 text-3xl md:text-5xl font-bold',
               styles.magic,
             )}
           >
             Shipping Policy
           </h1>
-          <Prose className="mb-8" html={formattedHtml} />
+          <Prose className="mb-8 text-justify" html={formattedHtml} />
         </div>
       </div>
       <Footer />
